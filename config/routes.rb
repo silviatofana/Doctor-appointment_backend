0149b5819c0a1f198030642db6df52c939771382
silviatofana <silvia.tofana@gmail.com>
2022-10-22 12:29:51 +0300

    add docotor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :doctors
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

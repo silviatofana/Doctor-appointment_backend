94bc2025485c28b4f640a0b2e1ffdf463f8d6520
Felix Ouma <fatonoh@gmail.com>
2022-10-24 16:49:49 +0300

    Add user authorization to endpoints

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :destroy, Appointment do |appointment|
+      appointment.user == user
+    end
+
+    can :update, Appointment do |appointment|
+      appointment.user == user
+    end
+
+    can :create, Appointment
+    can :create, Doctor
+  end
+end

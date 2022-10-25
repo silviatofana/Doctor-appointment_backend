561ef87a66872fa506b4c5b07ddf4a9b8bcac186
silviatofana <silvia.tofana@gmail.com>
2022-10-25 22:13:29 +0300

    fix linters

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Appointment, type: :model do
   subject do
     user = User.create(name: 'Solo Boss', password: '123456', email: 'soloboss@gmail.com')
     doctor = Doctor.create(name: 'Silvia', picture: 'https://icon.com/icon.jpg', specialization: 'Cardiologist')
-    Appointment.new(description: 'head ache', user: user, doctor: doctor, date_of_appointment: '2021-01-01', time_of_appointment: '10:00')
+    Appointment.new(description: 'head ache', user:, doctor:, date_of_appointment: '2021-01-01',
+                    time_of_appointment: '10:00')
   end
 
   before { subject.save }
@@ -25,18 +26,18 @@ RSpec.describe Appointment, type: :model do
     end
 
     it 'Appointment_time should be present' do
-        subject.time_of_appointment = nil
-        expect(subject).to_not be_valid
-      end
+      subject.time_of_appointment = nil
+      expect(subject).to_not be_valid
+    end
 
-      it 'Appointment_user should be present' do
-        subject.user_id = nil
-        expect(subject).to_not be_valid
-      end
+    it 'Appointment_user should be present' do
+      subject.user_id = nil
+      expect(subject).to_not be_valid
+    end
 
-      it 'Appointment_doctor should be present' do
-        subject.doctor_id = nil
-        expect(subject).to_not be_valid
-      end
+    it 'Appointment_doctor should be present' do
+      subject.doctor_id = nil
+      expect(subject).to_not be_valid
+    end
   end
 end

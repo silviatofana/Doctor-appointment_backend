c6d52f08a51f210011eb99bf27669f55185ffe70
silviatofana <silvia.tofana@gmail.com>
2022-11-01 13:09:10 +0200

    update doctor.rb

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,5 +2,15 @@ class Doctor < ApplicationRecord
   has_many :appointments
   has_many :users, through: :appointments
   validates :name, presence: true
+  validates :gender, presence: true
   validates :specialization, presence: true
+
+  def default_values
+    self.picture = if gender == 'M' || gender == 'Male'
+                     'male-placeholder.png'
+                   else
+                     'female-placeholder.png'
+                   end
+    picture
+  end
 end
